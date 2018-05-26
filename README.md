f13ffa6379e0538c9632b17d8e44a24cf8abc2f2
Shinichi Kogai <kogai0121@gmail.com>
2018-05-26 21:27:00 +0900

    Preparation

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 ```rs
+// 1. Manually convert from file to aribitarly type
+#[derive(Embeddable)]
+struct Wrapped(T);
+// or impl Embeddable for Wrapped {...};
+let config: T = Wrapped::with_embedded_file("conf.yml");
 let config = Config::with_file("conf.yml");
+
 // Embeded data of file into executable
 
 // 1. Impl Trait by auther of crate(very ideal)
 // #[derive(Embeddable)] or impl Embeddable for T {...}
 // fn with_embeded() -> Any {}
 let config = Config::with_embeded_file("conf.yml");
-
-// 1. Or manually convert from file to aribitarly type
-let config: T = Embeddable::from_type::<T>("conf.yml);
 ```
+
+- [ ] yaml
+- [ ] json
+- [ ] toml
+- [ ] xml
+
+- [ ] log4rs
+- [ ] clap-rs
+
+- [ ] nightly
+- [ ] 1.26.0
+- [ ] 1.25.0
+- [ ] 1.24.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,43 @@
+extern crate serde;
+
+#[cfg(test)]
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use std::path::Path;
+
+trait Embeddable {
+    fn from_file<P: AsRef<Path>>(path: P) -> Self;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn from_file() {
+        use std::fs;
+        use std::io::Read;
+
+        #[derive(Deserialize, Serialize, PartialEq, Debug)]
+        struct T {
+            version: usize,
+        };
+        impl Embeddable for T {
+            fn from_file<P: AsRef<Path>>(path: P) -> Self {
+                let x = match fs::File::open(path) {
+                    Ok(mut file) => {
+                        let mut buf = Vec::new();
+                        let _ = file.read_to_end(&mut buf);
+                        buf
+                    }
+                    Err(e) => unreachable!(e),
+                };
+                let x = serde_json::from_slice::<T>(&x).unwrap();
+                x
+            }
+        }
+        assert_eq!(T { version: 1 }, T::from_file("assets/simple.json"));
     }
 }

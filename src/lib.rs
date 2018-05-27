7ec6ad330a51f9314cbf310761ffb6be873ec3c5
Shinichi Kogai <kogai0121@gmail.com>
2018-05-27 13:06:06 +0900

    Attempt to use doctest

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,55 @@
 extern crate serde;
-
-#[cfg(test)]
-#[macro_use]
-extern crate quote;
-
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
-use serde::{Deserialize, Serialize};
-
+/// A macro generates from arbitrary file path and type definition which involve to the file to the data structure ebeddeded data of file.
+///
+/// # Example
+///
+/// ```
+/// # extern crate serde;
+/// # #[macro_use]
+/// # extern crate serde_derive;
+/// # extern crate serde_json;
+/// # #[macro_use]
+/// # extern crate embeddable_config;
+/// # fn main() {
+///
+/// #[derive(Deserialize, PartialEq, Debug)]
+/// struct Sample {
+///     version: usize,
+/// }
+///
+/// let instance = embedded!("../assets/simple.json", Sample);
+/// assert_eq!(Sample { version: 1 }, instance.value().unwrap());
+/// # }
+/// ```
 #[macro_export]
 macro_rules! embedded {
     ($path:expr, $def:ty) => {{
-        pub struct EmbeddedT {
+        use serde::Deserialize;
+
+        pub struct Embedded {
             internal: &'static [u8],
         }
 
-        impl EmbeddedT {
-            pub fn new() -> Self {
-                EmbeddedT {
+        impl Embedded {
+            fn new() -> Self {
+                Embedded {
                     internal: include_bytes!($path),
                 }
             }
 
-            pub fn value<'a, T>(&self) -> T
+            pub fn value<'a, T>(&self) -> serde_json::Result<T>
             where
                 T: serde::Deserialize<'a>,
             {
-                serde_json::from_slice(&self.internal).unwrap()
+                serde_json::from_slice(&self.internal)
             }
         }
 
-        EmbeddedT::new()
+        Embedded::new()
     }}
 }
 
@@ -40,7 +57,7 @@ macro_rules! embedded {
 mod tests {
     use super::*;
 
-    #[derive(Deserialize, Serialize, PartialEq, Debug)]
+    #[derive(Deserialize, PartialEq, Debug)]
     struct Sample {
         version: usize,
     }
@@ -48,6 +65,6 @@ mod tests {
     #[test]
     fn from_file() {
         let instance = embedded!("../assets/simple.json", Sample);
-        assert_eq!(Sample { version: 1 }, instance.value());
+        assert_eq!(Sample { version: 1 }, instance.value().unwrap());
     }
 }

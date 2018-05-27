a8e41de1f087d7e548680b852d966caf4b4c3b09
Shinichi Kogai <kogai0121@gmail.com>
2018-05-27 12:25:58 +0900

    First PoC

--- src/lib.rs
+++ src/lib.rs
@@ -9,36 +9,45 @@ extern crate serde_derive;
 extern crate serde_json;
 
 use serde::{Deserialize, Serialize};
-#[derive(Deserialize, Serialize, PartialEq, Debug)]
-struct Sample {
-    version: usize,
-}
 
-pub struct EmbeddedT {
-    internal: &'static [u8],
-}
-
-impl EmbeddedT {
-    pub fn new() -> Self {
-        EmbeddedT {
-            internal: include_bytes!("../assets/simple.json"),
+#[macro_export]
+macro_rules! embedded {
+    ($path:expr, $def:ty) => {{
+        pub struct EmbeddedT {
+            internal: &'static [u8],
         }
-    }
-    pub fn value<'a, T>(&self) -> T
-    where
-        T: serde::Deserialize<'a>,
-    {
-        serde_json::from_slice(&self.internal).unwrap()
-    }
+
+        impl EmbeddedT {
+            pub fn new() -> Self {
+                EmbeddedT {
+                    internal: include_bytes!($path),
+                }
+            }
+
+            pub fn value<'a, T>(&self) -> T
+            where
+                T: serde::Deserialize<'a>,
+            {
+                serde_json::from_slice(&self.internal).unwrap()
+            }
+        }
+
+        EmbeddedT::new()
+    }}
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[derive(Deserialize, Serialize, PartialEq, Debug)]
+    struct Sample {
+        version: usize,
+    }
+
     #[test]
     fn from_file() {
-        let z = EmbeddedT::new();
-        assert_eq!(Sample { version: 1 }, z.value());
+        let instance = embedded!("../assets/simple.json", Sample);
+        assert_eq!(Sample { version: 1 }, instance.value());
     }
 }
